f4bce7ee2fc6027d6f48472fae2121b1a23f6e52
Artfal11 <ilshat3092005@gmail.com>
2025-06-17 12:18:46 +0300

    Test workflow 2

--- backend/ingredients/admin.py
+++ backend/ingredients/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, RecipeIngredient
+from .models import Ingredient
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
